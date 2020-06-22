7c7d62a744f5ec3970b6bb341b51e0e887e4b987
Baltazar Ortiz <bortiz19@mail.wou.edu>
2020-06-22 00:00:49 -0700

    msg

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-let def = true;
+let emptyScreen = true;
 let newToken = true;
-let operation = ["0"];
-let token = "0";
-let screen = document.querySelector("#calc_screen").value = "0";
+let screen = document.querySelector("#calc_screen");
+screen.value = "0"
 
 const display = (op) => {
-    if ( op === '.' ) {
-        if (newToken) {
-            screen.value += op;
-            token += op;
-            operation.pop();
-            operation.push(token);
-            newToken = false;
-        }
+    if (op === '.' && newToken) {   // if no recorded decimal in token
+        screen.value += op;         // put .
+        newToken = false;           // now has decimal in token
     } else {
-        if ( isMathOp(op) ) {
-            operation.pop();
-            operation.push(token,op);
-            screen.value += op;
-            newToken = true;
+        if (isMathOp(op)) {       // add math , +, -, /, *
+            screen.value += op;     // add screen val
+            newToken = true;        // ready for new number
         } else {
-                // numbers here
+            if (isNumberic(op)) {           // adding number
+                if (emptyScreen) {          // if empty screen eligible, make it empty screen
+                    emptyScreen = false;
+                    screen.value = "";
+                }
+                screen.value += op;         // add number to screen
+            }
         }
     }
 };
@@ -29,31 +27,51 @@ const isNumberic = (a) => !isNaN(a);
 
 const clearScreen = () => {
     screen.value = "0";
-    def = true;
+    emptyScreen = true;
 };
 
 const solve = () => {
-    screen.querySelector("#calc_screen").value = "0"
+    let mathTokens = screen.value.split(" ");
+    let i = 0;
+    let higherOpDone = false;
+    for (let i = 0; i < 2; i++) {
+        while (i < mathTokens.length) {
+            if (isMathOps(mathTokens[i])) {
+                let a = parseInt(mathTokens[i - 1]);
+                let b = parseInt(mathTokens[i + 1]);
+                let c = null;
+                if (!higherOpDone && higherOp(mathTokens[i])) {
+                    if (mathTokens[i] === "*") {
+                        c = a * b;
+                    } else {
+                        c = a / b;
+                    }
+                    screen.value = c;
+                    mathTokens[i + 1] = c.toString();
+                    mathTokens.splice(i - 1, 2);
+                } else if (higherOpDone) {
+                    if (mathTokens[i] === "+") {
+                        c = a + b;
+                    } else {
+                        c = a - b;
+                    }
+                    screen.value = c;
+                    mathTokens[i + 1] = c.toString();
+                    mathTokens.splice(i - 1, 2);
+                }
+            }
+            i++;
+        }
+        higherOpDone = true;
+    }
 };
 
 const isMathOp = (op) => {
-    return op === ' + ' || op === ' - ' || op === ' / ' || op === ' * ';
+    return op === ' + ' || op === ' - ' || op === ' / ' || op === ' * ';    // need spaces for splitting
 };
 
-// function display(oper) {
-//     let screen = document.querySelector("#calc_screen");
-//     if (def) {
-//         def = false;
-//         screen.value = oper;
-//         operation.push()
-//     } else {
-//         if (oper === ".") {
-//             if (!screen.value.includes("."))
-//                 screen.value += oper;
-//         } else if (isNumberic(oper)) {
-//             screen.value += oper;
-//         } else {
-//             screen.value += " " + oper + " ";
-//         }
-//     }
-// }
+const isMathOps = (op) => {
+    return op === '+' || op === '-' || op === '/' || op === '*';    // need spaces for splitting
+};
+
+const higherOp = (operand) => operand === "*" || operand === "/";
