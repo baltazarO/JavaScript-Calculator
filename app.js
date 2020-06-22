f4a950c2dd159198350a4c83908f624a328ac9fd
Baltazar Ortiz <bortiz19@mail.wou.edu>
2020-06-22 01:25:20 -0700

    Functional calculator

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-let emptyScreen = true;
+let brandNewCalc = true;
 let newToken = true;
 let screen = document.querySelector("#calc_screen");
 screen.value = "0"
+let justCalculated = false;
 
 const display = (op) => {
+    clearScreenIfNeed(op);
     if (op === '.' && newToken) {   // if no recorded decimal in token
         screen.value += op;         // put .
         newToken = false;           // now has decimal in token
@@ -11,14 +13,9 @@ const display = (op) => {
         if (isMathOp(op)) {       // add math , +, -, /, *
             screen.value += op;     // add screen val
             newToken = true;        // ready for new number
+            justCalculated = false;
         } else {
-            if (isNumberic(op)) {           // adding number
-                if (emptyScreen) {          // if empty screen eligible, make it empty screen
-                    emptyScreen = false;
-                    screen.value = "";
-                }
-                screen.value += op;         // add number to screen
-            }
+            screen.value += op;         // add number to screen
         }
     }
 };
@@ -27,44 +24,64 @@ const isNumberic = (a) => !isNaN(a);
 
 const clearScreen = () => {
     screen.value = "0";
-    emptyScreen = true;
+    brandNewCalc = true;
 };
 
 const solve = () => {
     let mathTokens = screen.value.split(" ");
-    let i = 0;
-    let higherOpDone = false;
-    for (let i = 0; i < 2; i++) {
-        while (i < mathTokens.length) {
-            if (isMathOps(mathTokens[i])) {
-                let a = parseInt(mathTokens[i - 1]);
-                let b = parseInt(mathTokens[i + 1]);
-                let c = null;
-                if (!higherOpDone && higherOp(mathTokens[i])) {
-                    if (mathTokens[i] === "*") {
-                        c = a * b;
-                    } else {
-                        c = a / b;
-                    }
-                    screen.value = c;
-                    mathTokens[i + 1] = c.toString();
-                    mathTokens.splice(i - 1, 2);
-                } else if (higherOpDone) {
-                    if (mathTokens[i] === "+") {
-                        c = a + b;
-                    } else {
-                        c = a - b;
-                    }
-                    screen.value = c;
-                    mathTokens[i + 1] = c.toString();
-                    mathTokens.splice(i - 1, 2);
-                }
-            }
-            i++;
-        }
-        higherOpDone = true;
-    }
+    solveHigherOps(mathTokens);
+    screen.value = solveLowerOps(mathTokens);
+    justCalculated = true;
 };
+
+function solveHigherOps(mathTokens) {
+    let i = 0;
+    let c = 0;
+    while (i < mathTokens.length) {
+        if (higherOp(mathTokens[i])) {
+            let a = parseInt(mathTokens[i - 1]);
+            let b = parseInt(mathTokens[i + 1]);
+            if (mathTokens[i] === "*") {
+                c += a * b;
+            } else {
+                c += a / b;
+            }
+            mathTokens[i + 1] = c.toString();
+            mathTokens.splice(i - 1, 2);
+        }
+        i++;
+    }
+}
+
+function solveLowerOps(mathTokens) {
+    let i = 0;
+    let c = 0;
+    while (i < mathTokens.length) {
+        if (isMathOps(mathTokens[i])) {
+            let a = parseInt(mathTokens[i - 1]);
+            let b = parseInt(mathTokens[i + 1]);
+            if (mathTokens[i] === "+") {
+                c += a + b;
+            } else {
+                c += a - b;
+            }
+            mathTokens[i + 1] = c.toString();
+            mathTokens.splice(i - 1, 2);
+        }
+        i++;
+    }
+    return mathTokens[0];
+}
+
+function clearScreenIfNeed(operation) {
+    if (isNumberic(operation)) {
+        if (justCalculated || brandNewCalc){
+            screen.value = "";
+            justCalculated = false;
+            brandNewCalc = false;
+        }
+    }
+}
 
 const isMathOp = (op) => {
     return op === ' + ' || op === ' - ' || op === ' / ' || op === ' * ';    // need spaces for splitting
